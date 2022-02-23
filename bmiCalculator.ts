4eb5fd0e975136314b39ebabf4d6c5b8b0dc8fd3
Kolade Afode <kolade.afode@yahoo.com>
2022-02-23 08:26:37 +0100

    complete exercises 9.5

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,10 +1,31 @@
-const calculateBmi = (height: number, weight: number) => {
+export const calculateBmi = (height: number, weight: number) => {
   const heightM = height / 100;
   const BMI = weight / (heightM * heightM);
-  if (BMI < 18.5) return "Underweight";
-  if (BMI >= 18.5 && BMI <= 24.9) return "Healthy Weight";
-  if (BMI >= 25.0 && BMI <= 29.9) return "Overweight";
-  if (BMI > 30.0) return "Obesity";
+  if (BMI < 18.5)
+    return {
+      weight: weight,
+      height: height,
+      bmi: "Underweight",
+    };
+  if (BMI >= 18.5 && BMI <= 24.9)
+    return {
+      weight: weight,
+      height: height,
+      bmi: "Healthy Weight",
+    };
+  if (BMI >= 25.0 && BMI <= 29.9)
+    return {
+      weight: weight,
+      height: height,
+      bmi: "Overweight",
+    };
+  if (BMI > 30.0)
+    return {
+      weight: weight,
+      height: height,
+      bmi: "Obesity",
+    };
+  return;
 };
 
 console.log(calculateBmi(Number(process.argv[2]), Number(process.argv[3])));
--- index.ts
+++ index.ts
@@ -1,8 +1,22 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator";
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  let weight = req.query.weight;
+  let height = req.query.height;
+
+  if (!weight || !height) {
+    res.send({
+      error: "malformatted parameters",
+    });
+  }
+  const value = calculateBmi(Number(height), Number(weight));
+  res.send(value);
 });
 
 const PORT = 3003;
